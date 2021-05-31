5747bb965129f7a12424c0d184fa244596e2f8b6
Giuseppe Cannone <giuseppecannone22@gmail.com>
2021-05-31 19:40:54 +0200

    prova commit

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   # test "connects with cookies" do
   #   cookies.signed[:user_id] = 42
-  #
+  #   esempio
   #   connect
   #
   #   assert_equal connection.user_id, "42"
